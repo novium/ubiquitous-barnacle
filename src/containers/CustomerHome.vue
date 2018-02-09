9df1fc1cd353c56697969d2427ed2f0d32c12dfd
Andreas Andersson <36001750+Afixe@users.noreply.github.com>
2018-02-09 14:41:39 +0100

    For myself.

--- src/containers/CustomerHome.vue
+++ src/containers/CustomerHome.vue
@@ -1,15 +1,28 @@
 <template>
   <div id="container">
+      <div id="testtext">  {{test}}  </div>
+      <div id="testtext">  {{test2}}  </div>
     <Map />
+
   </div>
 </template>
 
 <script>
+
+  
+
   import Map from '../components/Map.vue'
 
   export default {
+    data(){
+      return{
+        test: "testy",
+        test2: "Testy the testicle"}
+      
+      },
+    
     components: {
-      Map
+      Map 
     }
   }
 </script>
@@ -18,5 +31,15 @@
   #container {
     width: 100%;
     height: 100%;
+    position: absolute;
+    z-index: -1;
+  }
+  #testtext {
+  position: absolute;  
+  display: absolute;
+  z-index: 1;
+  
+
+
   }
 </style>
