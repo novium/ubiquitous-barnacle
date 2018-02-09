f80a3c40c09ab2b07d8ab364ab8fc655320b07cd
Mattias Ahlsen <mattias.ahlsen@gmail.com>
2018-02-09 14:19:16 +0100

    Added bootstrap and dispatcher.

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 
+import BootstrapVue from 'bootstrap-vue';
+
 window.mapboxgl = require('mapbox-gl');
 mapboxgl.accessToken = 'pk.eyJ1Ijoibm92aXVtIiwiYSI6ImNpeGFmMzl5MzAwMTkydG54Nmw0N3Z6eGoifQ.awDpSy43luwX5rm1W8RSKA';
 
@@ -8,6 +10,9 @@ import App from './App.vue';
 
 import Customer from './containers/Customer.vue';
 import CustomerHome from './containers/CustomerHome.vue';
+
+import Dispatcher from './containers/Dispatcher.vue';
+import DispatcherHome from './containers/DispatcherHome.vue';
 
 import Home from './components/Home.vue';
 
@@ -20,7 +25,11 @@ const routes = [
     children: [
       { path: '', component: CustomerHome }
     ]
-  }
+  },
+  { path: '/dispatcher', component: Dispatcher,
+    children: [
+      { path: '', component: DispatcherHome }
+    ]}
 ];
 
 const router = new VueRouter({
